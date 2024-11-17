3b66d829336220b4230a2a7af7205f4b21b2b9e8
jemmons27 <126831018+jemmons27@users.noreply.github.com>
2024-11-17 11:44:33 -0600

    Added logic for understanding process/time request

--- extract_ingredient.py
+++ extract_ingredient.py
@@ -50,15 +50,71 @@ def quantity_find_process(task, ingredients):
     return
 
 
-def time_find_process(task, ingredients):
+def time_find_process(task, ingredients, processes=[], step=''):
+    if step == '':
+        step = "Add cumin, paprika, turmeric, oregano, black pepper, and cayenne and cook, stirring, for another 30 to 60 seconds."
+    if processes == []:
+        processes = ['boil', 'broil', 'bake', 'cook', 'sear']
     ##maybe makes sense to add temperature here
-    has_process = re.compile(r'\b(how long|how many|when)\s+(.*)\b', re.IGNORECASE)
-    process_search = re.search(has_process, task)
-    print(process_search.group(2))
-    if process_search:
-        print("Find time for", process_search.group(2))
+    
+    ##has_time_and_unit = re.compile(r'\b\d+\b\s*(minute|minutes|second|seconds|hour|hours|day|days)\b', re.IGNORECASE)
+    ##time_unit_search = re.search(has_time_and_unit, task)
+    
+    has_ingredient = re.compile(r'\b(how long|how many|when)\s+(.*)\b', re.IGNORECASE)
+    has_ingredient_search = re.search(has_ingredient, task)
+    closest_match = {}
+    if has_ingredient_search:
+        ## This is a bit messy if processes and ingredient names are similar, i.e. boil vs olive oil
+        max = {'diff': 0, 'ing': {}}
+        #Find closest ingredient
+        for ing in ingredients:
+            res = []
+            words_to_keep = ing['name'].split()
+            for word in words_to_keep:
+                if any(word in task_word for task_word in has_ingredient_search.group(2).split()):
+                    res.append(word)
+            # Filter the second string to include only words that are in the set
+            filtered_string = ' '.join(res)
+        
+            # Check result against ingredient name
+            if filtered_string:
+                diff = SequenceMatcher(None, ing['name'], filtered_string).ratio()
+                if diff > max['diff']:
+                    max = {'diff': diff, 'ing': ing}
+                
+                
+        closest_match = max['ing']
+        print('Is this the correct ingredient?', closest_match)
+    has_process = re.compile(r'\b(do i|should i|can i|done|is)\s+(.*)\b', re.IGNORECASE)
+    has_process_search = re.search(has_process, task)
+    process_guess = {}
+    if has_process:
+        max = {'diff': 0, 'process': ''}
+        print(has_process_search.group(2))
+        for process in processes:
+            res = []
+            words_to_keep = process.split()
+            for word in words_to_keep:
+                if any(word in task_word for task_word in has_process_search.group(2).split()):
+                    res.append(word)
+            filtered_string = ' '.join(res)
+            if filtered_string:
+                diff = SequenceMatcher(None, process, filtered_string).ratio()
+                if diff > max['diff']:
+                    max = {'diff': diff, 'process': process}
+        process_guess = {'process':max['process'], 'time': 0}
+        print("Is this the correct process?",process_guess)
+    if (closest_match):
+        if (process_guess):
+        ## If we have a guess for ingredient and process, can find exact step being referred to and return that step
+            print('Find step with both closest_match and process_guess, then return')
+        else:
+            print('Find all steps about ingredient=closest_match. If 1, return that. If more than 1, find which contains either numbers or time related words')
+    elif (process_guess):
+        print('Find all steps which include this process. If there is a reference to an ambiguous ingredient (that, this, it, etc), refer to previous step, otherwise do the same as above')
     else:
-        print('no process found')
+        print('No reasonable guesses ')
+    ## No ingredients, find best guess from task/steps
     return
 
 
