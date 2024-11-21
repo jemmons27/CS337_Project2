3581847e70b7e79ab6de64b640a30a02da4b7c1a
Jasmine Meyer <110313151+jasmey@users.noreply.github.com>
2024-11-21 14:52:34 -0600

    fixed temperature handling (nlp(doc) implementation wonky)

--- temperature_query.py
+++ temperature_query.py
@@ -4,29 +4,39 @@ import spacy
 def find_temperature(task, steps, current_step, ingredients, last_query):
    print("This has been interpreted as a temperature related task")
    nlp = spacy.load('en_core_web_sm')
-   doc = nlp(task)
-   tools = []
-   for token in doc:
-      ##Tools referenced in task are usually nsubj
-      if token.dep_ == 'nsubj':
-         tools.append(token.text)
+   tools = re.findall(r'\b(oven|stove|burner|pot|pan|grill|microwave)\b', task, re.IGNORECASE)
    ##Do we need to specify Celsius vs fahrenheit if both are in the recipe?
    temperature_re = re.compile(r'\b(\d+)\s*((degrees|°) (F|C|Farenheit|Celsius))\b', re.IGNORECASE)
+   # Regular expression to match temperature levels
+   level_re = re.compile(r'\b(high|medium(?: |-)high|medium(?: |-)low|low)\b', re.IGNORECASE)
+   
    temperature_match = re.search(temperature_re, steps[current_step-1]['step'])
    if temperature_match:
       temperature = temperature_match.group(1)
       unit = temperature_match.group(2)
       res = ''
-      if tools != []:
-         res = res + 'Set the ' + tools[0] + " to "
-      if temperature != '':
-         res = res + temperature + ' '
-      if unit != '':
-         res = res + unit
+      if tools:
+            res += 'Set the ' + tools[0] + " to "
+      if temperature:
+         res += temperature + ' '
+      if unit:
+         res += unit
       print(res)
       last_query['query'] = task
       last_query['output'] = res
    else:
-      print("Couldn't find a temperature in this step, try another query or navigate to another step")
-      
+      # Search for temperature levels in the current step
+        level_match = re.search(level_re, steps[current_step-1]['step'])
+        if level_match:
+            level = level_match.group(1)
+            res = ''
+            if tools:
+                res += 'Set the ' + tools[0] + " to "
+            res += level
+            print(res)
+            last_query['query'] = task
+            last_query['output'] = res
+        else:
+            print("Couldn't find a temperature in this step, try another query or navigate to another step")
+    
    return current_step, last_query
