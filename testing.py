9ce40d6c02895d678971788aae3074409a6a3dbb
jemmons27 <126831018+jemmons27@users.noreply.github.com>
2024-11-21 17:21:59 -0600

    Nice!

--- testing.py
+++ testing.py
@@ -7,9 +7,9 @@ import spacy
 def init_test():
     #url = 'https://www.allrecipes.com/shakshuka-for-one-recipe-8584907'
     #url = 'https://www.allrecipes.com/recipe/26107/best-breakfast-cookie/'
-    #url = 'https://www.allrecipes.com/spicy-turkey-gumbo-recipe-8744687' # url contains the allrecipes url being used
+    url = 'https://www.allrecipes.com/spicy-turkey-gumbo-recipe-8744687' # url contains the allrecipes url being used
     #url = 'https://www.allrecipes.com/recipe/217328/jonagolds-chicken-vindaloo/'
-    url = 'https://www.allrecipes.com/recipe/85452/homemade-black-bean-veggie-burgers/'
+    #url = 'https://www.allrecipes.com/recipe/85452/homemade-black-bean-veggie-burgers/'
     ## Initial parsing of recipe in web_scraping.py
     soup, steps, ingredients = web_scraping.fetch_recipe(url)
     task = '' # Task holds the current query, and is overwritten each time a new query is input
@@ -18,19 +18,22 @@ def init_test():
     current_step = 1
     ## curr_step is an int marking current step. Always use curr_step-1 when indexing steps, as "step 1" corresponds to steps[0]
     
-    print('Welcome to the Recipe Bot! Press enter to start the recipe. Type "0" to exit.')
-    input()
-    print('You need the following ingredients:')
-    for ingredient_info in ingredients:
-        print(ingredient_info['quantity'] + " " + ingredient_info['unit'] + " " + ingredient_info['name'])  
+    #print('Welcome to the Recipe Bot! Press enter to start the recipe. Type "0" to exit.')
+    #input()
+    #print('You need the following ingredients:')
+    #for ingredient_info in ingredients:
+    #    print(ingredient_info['quantity'] + " " + ingredient_info['unit'] + " " + ingredient_info['name'])  
 
-    print('\n')
-    input('Press enter to start cooking!')
-    print('Get ready to cook!\n\n')
+    #print('\n')
+    #input('Press enter to start cooking!')
+    #print('Get ready to cook!\n\n')
+    last_query['output'] = steps[current_step-1]["step"]
+
     while current_step <= len(steps) and task != '0':
         print(f'||| Step {current_step}: {steps[current_step-1]["step"]}\n\n')
         print(f'Ask a question or ask me to move to a different step!')
         task = input('||| Type here > ')
+        
         #### Navigate and resolve task, heading first to task_navigation.py at the direct_task function
         current_step, last_query = task_navigation.direct_task(task, soup, current_step, steps, ingredients, last_query)
     print("You've completed the recipe! Enjoy your meal!")
@@ -58,6 +61,8 @@ def main():
     while current_step <= len(steps) and task != '0':
         print(f'||| Step {current_step}: {steps[current_step-1]["step"]}\n\n')
         print(f'Ask a question or ask me to move to a different step!')
+        last_query['output'] = steps[current_step-1]['step']
+        
         task = input('||| Type here > ')
         #### Navigate and resolve task, heading first to task_navigation.py at the direct_task function
         current_step, last_query = task_navigation.direct_task(task, soup, current_step, steps, ingredients, last_query)
@@ -67,7 +72,7 @@ def main():
     
     
 if __name__ == "__main__":
-    main()
+    init_test()
     
     
 ###****UNIMPLEMENTED****
